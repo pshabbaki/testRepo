c8c156a432f18b44247b9b92bfdc688c12be3c8d
pshabbaki <pshabbaki24@gmail.com>
2020-05-23 16:24:12 -0700

    Updates

---
+++ precache-manifest.770ff7d7e09cc3cfa4431d3d29019cca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b2b9f5be88566949782a51b88c381d",
+    "url": "/testRepo/index.html"
+  },
+  {
+    "revision": "38e398b7e063ee5ea02d",
+    "url": "/testRepo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4aa65944816f12daa330",
+    "url": "/testRepo/static/js/2.f06179cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testRepo/static/js/2.f06179cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e398b7e063ee5ea02d",
+    "url": "/testRepo/static/js/main.24cfd0cd.chunk.js"
+  },
+  {
+    "revision": "8ac8c31be708471371dd",
+    "url": "/testRepo/static/js/runtime-main.be6dae32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testRepo/static/media/logo.5d5d9eef.svg"
+  }
+]);
