00bacb17ed1db10719ab33f5a9f8d84090495db5
pshabbaki <pshabbaki24@gmail.com>
2020-05-23 17:05:43 -0700

    adding redux

---
+++ src/store/createStore.ts
@@ -0,0 +1,26 @@
+import {
+    applyMiddleware,
+    compose,
+    createStore
+} from "redux";
+import thunk from "redux-thunk";
+
+import rootReducer from "../reducers";
+
+export default function createReduxStore() {
+    const anyWindow = window as any;
+
+    const reduxDevTools =
+        anyWindow.__REDUX_DEVTOOLS_EXTENSION__
+            ? anyWindow.__REDUX_DEVTOOLS_EXTENSION__()
+            : (cmpse: any) => cmpse;
+
+        return createStore(
+            rootReducer,
+            {},
+            compose(
+                applyMiddleware(thunk),
+                reduxDevTools,
+            )
+        )
+}
