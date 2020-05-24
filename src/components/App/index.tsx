0e4bc3378504ce22b54e57d1cf7f327694b51384
pshabbaki <pshabbaki24@gmail.com>
2020-05-23 19:09:36 -0700

    adding website

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -8,16 +8,8 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bah bah amo Mottaghi. how are yout kojas?
+          Bah bah amo Mokhtari. how are yout kojas?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
